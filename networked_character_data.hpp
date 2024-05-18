b418b641a53e77ef0d90c4788331daecd20d32d9
cuppajoeman <ccn@cuppajoeman.com>
2024-05-18 04:00:07 -0400

    add default value for edge case

--- networked_character_data.hpp
+++ networked_character_data.hpp
@@ -6,7 +6,9 @@
 struct NetworkedCharacterData {
     uint64_t client_id;
     // cihtems: client_input_history_insertion_time_epoch_ms
-    int cihtems_of_last_server_processed_input_snapshot;
+    // it's possible to have this property as -1 if the server sends a
+    // game update out before the first input has been processed for this player.
+    int cihtems_of_last_server_processed_input_snapshot = -1;
     float character_x_position;
     float character_y_position;
     float character_z_position;
