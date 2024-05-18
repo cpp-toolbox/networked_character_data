a576f36d0b6bc4e410f0e5f350cafb9a2505edc4
cuppajoeman <ccn@cuppajoeman.com>
2024-05-18 03:31:57 -0400

    initial files

---
+++ networked_character_data.hpp
@@ -0,0 +1,17 @@
+#ifndef NETWORKED_CHARACTER_DATA_HPP
+#define NETWORKED_CHARACTER_DATA_HPP
+
+#include <cstdint>
+
+struct PlayerData {
+  uint64_t client_id;
+  // cihtems: client_input_history_insertion_time_epoch_ms
+  int cihtems_of_last_server_processed_input_snapshot;
+  float character_x_position;
+  float character_y_position;
+  float character_z_position;
+  double camera_yaw_angle;
+  double camera_pitch_angle;
+};
+
+#endif
